efb501c96e4932ffe60e0c6908e643031f2aa742
meng hor <piseth125125@gmail.com>
2024-09-25 16:02:50 +0700

    install tailwinds

---
+++ js/js_event.php
@@ -0,0 +1,19 @@
+<?php
+echo 'hello php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello js </title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.3/dist/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <div class=" bg-red-300">
+        hell js 
+    </div>
+</body>
+</html>
