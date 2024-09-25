74c6fedba9c4d50d47356d2f4619d8bf89769141
meng hor <piseth125125@gmail.com>
2024-09-25 15:47:39 +0700

    firs commit

---
+++ js_event.php
@@ -0,0 +1,17 @@
+<?php
+echo 'hello php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello js </title>
+</head>
+<body>
+    <div>
+        hell js 
+    </div>
+</body>
+</html>
