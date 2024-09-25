aad6ecc15119b01ce3cd4c1c08db077004e5c135
meng hor <piseth125125@gmail.com>
2024-09-25 16:22:35 +0700

    fix: file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './**/*.php', // Include all PHP files
+    './**/*.html', // Include all HTML files
+    './js/**/*.js', // Include all JS files in the js folder
+    './dist/**/*.css', // Include all CSS files in the dist folder if needed
+    // Add any other paths where you use Tailwind classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
