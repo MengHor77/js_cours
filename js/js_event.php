4c8a2e3f7912c385ecb6326efec680dd77b6510d
meng hor <piseth125125@gmail.com>
2024-09-25 16:30:12 +0700

    fix: css

--- js/js_event.php
+++ js/js_event.php
@@ -9,10 +9,11 @@ echo 'hello php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hello js </title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.3/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../dist/styles.css">
 
 </head>
 <body>
-    <div class=" bg-red-300">
+    <div class=" bg-red-700">
         hell js 
     </div>
 </body>
